21f6892cdcaf859f8e166f7544e123a3c2aca501
Damian Gonzalez <dagosalgado18@gmail.com>
2023-05-10 22:20:53 -0600

    Fin seccion 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { InfoPaginaService } from './services/info-pagina.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(public _infoPagina: InfoPaginaService){
+    //leer archivo json
+    
+
+
+
+
+
+  }
+}
